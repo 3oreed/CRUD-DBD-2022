3f882c2092353c9e7550c62371369414e16d9241
Byron Caices <byron.caices@usach.cl>
2022-12-10 16:08:06 -0300

    cliente_tienda imp+repo LISTOS

--- demo/src/main/java/dbd/LAB/crud/repositories/Cliente_tiendaImp.java
+++ demo/src/main/java/dbd/LAB/crud/repositories/Cliente_tiendaImp.java
@@ -1,4 +1,81 @@
 package dbd.LAB.crud.repositories;
 
-public class Cliente_tiendaImp {
+import dbd.LAB.crud.models.Cliente_tienda;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
+import java.util.List;
+
+public class Cliente_tiendaImp implements Cliente_tiendaRepository{
+    private Sql2o sql2o;
+
+    @Override
+    public Cliente_tienda crear(Cliente_tienda clienteTienda) {
+        try (Connection conn = sql2o.open()){
+            String sql = "INSERT INTO cliente_tienda (id_cliente,id_tienda,edad_minima)" +
+                    "VALUES (:id_cliente,:id_tienda,:edad_minima)";
+            conn.createQuery(sql,true)
+
+                    .addParameter("id_cliente",clienteTienda.getId_cliente())
+                    .addParameter("id_tienda",clienteTienda.getId_tienda())
+                    .addParameter("edad_minima",clienteTienda.getEdad_minima())
+
+                    .executeUpdate();
+            return clienteTienda;
+
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    @Override
+    public String update(Cliente_tienda clienteTienda, int id_cliente_tienda) {
+        try(Connection conn = sql2o.open()){
+            String updateSql = "update cliente_tienda set id_cliente_tienda=:id_cliente_tienda";
+            conn.createQuery(updateSql)
+                    .addParameter("id_cliente_tienda",clienteTienda.getId_cliente_tienda())
+                    .executeUpdate();
+            return "Se actualizo id_cliente_tienda de un Cliente_tienda";
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            return "Fallo al actualizar id_cliente_tienda de Cliente_tienda";
+        }
+
+    }
+
+    @Override
+    public List<Cliente_tienda> getAll() {
+        try(Connection conn = sql2o.open()){
+            return conn.createQuery("select * from cliente_tienda order by id_cliente_tienda asc ")
+                    .executeAndFetch(Cliente_tienda.class);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
+    @Override
+    public List<Cliente_tienda> show(int id_cliente_tienda) {
+        try(Connection conn = sql2o.open()){
+            return conn.createQuery("SELECT * from cliente_tienda where id_cliente_tienda=:id_cliente_tienda ")
+                    .addParameter("id_cliente_tienda ",id_cliente_tienda)
+                    .executeAndFetch(Cliente_tienda.class);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
+    @Override
+    public void delete(int id_cliente_tienda) {
+        try(Connection conn = sql2o.open()){
+            conn.createQuery("DELETE from cliente_tienda where id_cliente_tienda=:id_cliente_tienda")
+                    .addParameter("id_cliente_tienda",id_cliente_tienda)
+                    .executeUpdate();
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
+    }
 }
--- demo/src/main/java/dbd/LAB/crud/repositories/Cliente_tiendaRepository.java
+++ demo/src/main/java/dbd/LAB/crud/repositories/Cliente_tiendaRepository.java
@@ -1,4 +1,17 @@
 package dbd.LAB.crud.repositories;
 
+import dbd.LAB.crud.models.Cliente_tienda;
+
+import java.util.List;
+
 public interface Cliente_tiendaRepository {
+    public Cliente_tienda crear(Cliente_tienda clienteTienda);
+
+    public String update(Cliente_tienda clienteTienda, int id_cliente_tienda);
+
+    public List<Cliente_tienda> getAll();
+
+    public List<Cliente_tienda> show(int id_cliente_tienda);
+
+    public void delete(int id_cliente_tienda);
 }
