0e9e1a4a98e56981bbdef876cc80a0926826cb7a
Byron Caices <byron.caices@usach.cl>
2022-12-09 21:05:50 -0300

    producto cat

---
+++ demo/src/main/java/dbd/LAB/crud/repositories/ProductoCategoriaRepository.java
@@ -0,0 +1,18 @@
+package dbd.LAB.crud.repositories;
+
+import dbd.LAB.crud.models.Empresa_Administrador;
+import dbd.LAB.crud.models.ProductoCategoria;
+
+import java.util.List;
+
+public interface ProductoCategoriaRepository {
+    public ProductoCategoria crear(ProductoCategoria ProductoCat);
+
+    public String update(ProductoCategoria ProductoCat, int idProdCat);
+
+    public List<ProductoCategoria> getAll();
+
+    public List<ProductoCategoria> show(int idProdCat);
+
+    public void delete(int idProdCat);
+}
