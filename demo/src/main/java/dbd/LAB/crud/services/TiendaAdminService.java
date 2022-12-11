96cab4d871379328fe2d9525c5c46fe0b81f5b7e
diegovaldes23 <diego.valdes.f@usach.cl>
2022-12-11 17:08:29 -0300

    tiendaadmin

--- demo/src/main/java/dbd/LAB/crud/services/TiendaAdminService.java
+++ demo/src/main/java/dbd/LAB/crud/services/TiendaAdminService.java
@@ -1,11 +1,24 @@
 package dbd.LAB.crud.services;
 import dbd.LAB.crud.models.TiendaAdmin;
 import dbd.LAB.crud.repositories.TiendaAdminRepository;
+
 import org.springframework.context.annotation.Lazy;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
 
 import java.util.List;
 
+@CrossOrigin
+@RestController
 public class TiendaAdminService implements TiendaAdminRepository {
 
     private final TiendaAdminRepository tiendaAdminRepository;
@@ -14,34 +27,35 @@ public class TiendaAdminService implements TiendaAdminRepository {
         this.tiendaAdminRepository = tiendaAdminRepository;
     }
 
-    @Override
+
     @PostMapping("/TiendaAdmin")
+    @ResponseBody
     public TiendaAdmin crear(@RequestBody TiendaAdmin tiendaAdmin) {
         TiendaAdmin resultado = tiendaAdminRepository.crear(tiendaAdmin);
         return resultado;
     }
 
-    @Override
-    @PutMapping("/TiendaAdmin")
+
+    @PutMapping("/TiendaAdmin/{id_tienda_admin}")
+    @ResponseBody
     public String update(@PathVariable TiendaAdmin tiendaAdmin,@PathVariable int id_tienda_admin) {
         String retorno = tiendaAdminRepository.update(tiendaAdmin,id_tienda_admin);
         return retorno;
     }
 
-    @Override
     @GetMapping("/TiendaAdmin")
     public List<TiendaAdmin> getAll() {
         return tiendaAdminRepository.getAll();
     }
 
-    @Override
+
     @GetMapping("/TiendaAdmin/{id_tienda_admin}")
     public List<TiendaAdmin> show(@PathVariable int id_tienda_admin) {
         return tiendaAdminRepository.show(id_tienda_admin);
     }
 
-    @Override
-    @DeleteMapping("/TiendaAdmin")
+
+    @DeleteMapping("/TiendaAdmin/{id_tienda_admin}")
     public void delete(@PathVariable int id_tienda_admin) {
         tiendaAdminRepository.delete(id_tienda_admin);
 
